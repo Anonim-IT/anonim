bb16aa17a7054610f366b5fac471a9103e080e78
Anonim <79274085+Anonim-IT@users.noreply.github.com>
2023-10-25 07:59:19 +0000

    new file:   com.sh 	modified:   ststart.sh

--- ststart.sh
+++ ststart.sh
@@ -1,2 +1,35 @@
 #!/bin/bash
-whiptail --title  "Test Message Box" --msgbox  "Create a message box with whiptail. Choose Ok to continue." 10 60
+###setfont cyr-sun16
+setfont ter-v32b
+
+#OPTION=$(whiptail --title  " LANGUAGE " --menu  "
+#
+#   " 15 60 4 \
+# "1" "Russian" \
+# "2" "English" 3>&1 1>&2 2>&3)
+ 
+# exitstatus=$?
+# if [ $exitstatus = 0 ];  
+#     then
+#         clear
+#         echo "" 
+#     else
+#         clear
+#         echo ""
+# fi
+
+# #-----------  Переход по выбору  ------------------------
+
+# case $OPTION in
+#                 "1")
+                sh -c "$(curl -fsSL https://raw.githubusercontent.com/Makar-Makarych/makar/main/t-efi-btr1.sh)"
+#              break
+#              ;;
+#                 "2")
+#                 sh -c "$(curl -fsSL https://raw.githubusercontent.com/Makar-Makarych/makar/main/t-efi-btr1-en.sh)"
+             
+#              break
+#              ;;
+#                 255)
+#      	        echo " ESC.";;
+# esac
