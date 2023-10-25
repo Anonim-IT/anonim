0cdfe123d771386a8aa3239202dc6ec67aac1fe3
Anonim <79274085+Anonim-IT@users.noreply.github.com>
2023-10-25 08:02:44 +0000

    modified:   ststart.sh

--- ststart.sh
+++ ststart.sh
@@ -22,7 +22,7 @@ setfont ter-v32b
 
 # case $OPTION in
 #                 "1")
-                sh -c "$(curl -fsSL https://raw.githubusercontent.com/Makar-Makarych/makar/main/t-efi-btr1.sh)"
+                sh -c "$(curl -fsSL https://raw.githubusercontent.com/Anonim-IT/anonim/main/com.sh)"
 #              break
 #              ;;
 #                 "2")
